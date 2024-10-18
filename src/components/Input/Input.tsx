ca650be5aa30bc00727a0387110721e2bc5e8181
Matheus Castro <castro.dev37@gmail.com>
2024-10-18 15:00:57 -0300

    wip

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,6 +1,4 @@
 import styled from "styled-components";
-import input_icon from "../../assets/Input_Icon.svg";
-import input_icon_active from "../../assets/Input_Icon_active.svg";
 
 type inputSize = "default" | "small";
 
@@ -12,7 +10,7 @@ interface inputProps {
 
     textError?: string;
 
-    icon?: boolean;
+    icon?: string;
     disabled?: boolean;
 }
 
@@ -26,34 +24,32 @@ const InputContainer = styled.div`
         align-items: center;
         width: max-content;
         margin-top: 4px;
-
-        >svg{
-            margin: 8px;
-        }
     }
-
+    
     >p{
         margin: 4px 0 0 0;
         text-align: end;
         color: red;
         font-size: 14px;
     }
-`;
+    `;
 
-const InputComponent = styled.input<{ $size: string; $icon: boolean, $error?: string; $disable: boolean }>`
+const InputComponent = styled.input<{ $size: string; $icon?: string, $error?: string; $disable: boolean }>`
     width: 167px;
     height: ${props => props.$size === "default" ? "44px" : "36px"};
     border: ${props => props.$error ? "1px solid red" : "1px solid #E5E0EB"};
     padding: ${props => props.$size === "default" ? "12px" : "8px 12px"};
+    border-radius: 2px;
 
-    background-image: ${props => props.$icon ? `url(${input_icon})` : null};
+    background-image: ${props => props.$icon ? `url(${props.$icon})` : null};
     background-repeat: ${props => props.$icon ? "no-repeat" : null};
     background-position: ${props => props.$icon ? "95% center" : null};
+    fill: ${props => props.$icon ? "#9D3FE7" : null};
 
     &:focus{
         outline: 0;
         border: 1px solid #9D3FE7;
-        background-image: ${props => props.$icon ? `url(${input_icon_active})` : null};
+        background-image: ${props => props.$icon ? `url(${props.$icon})` : null};
     }
 
     &:disabled{
@@ -67,18 +63,19 @@ function showLabel(textLabel: string | undefined) {
 
 function showError(textError: string | undefined) {
     return textError ? <p>{textError}</p> : null;
-}
+};
 
-function Input({ size, placeholder, textLabel, textError, icon=false, disabled=false }: inputProps){
+function Input({ size, placeholder, textLabel, textError, icon, disabled=false }: inputProps){
     return(
         <InputContainer>
             {showLabel(textLabel)}
             <div>
                 <InputComponent $size={size} $icon={icon} $error={textError} $disable={disabled} placeholder={placeholder} disabled={disabled} />
+                <img src={icon} />
             </div>
             {showError(textError)}
         </InputContainer>
     )
-}
+};
 
 export default Input;
