ee71424f238c7691eb7b4febee530305804f2f27
henrique-olivier <henrique.rosa@sptech.school>
2024-10-21 09:15:46 -0300

    corrigindo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
-import Button from "./components/Button";
-import icon from "./assets/save.svg"
-
 export default function App () {
-    return  (
-    <Button size='small' 
-    type='text'
-    icon={icon}
-    > 
-      <span>Button</span>
-    </Button> 
-    )
+  return (
+    <p>Hello Word</p>
+  )
   }
