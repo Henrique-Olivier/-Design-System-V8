ecd5ad9cd3267fcd3bbbe118836e6634233007d6
Matheus Castro <castro.dev37@gmail.com>
2024-10-22 10:49:32 -0300

    finalizado componente typography

--- src/components/Typography/typography.tsx
+++ src/components/Typography/typography.tsx
@@ -1,46 +1,49 @@
 import styled from "styled-components";
 
+type size = "desktop" | "tablet" | "mobile" | "main";
 type headTag = "H1" | "H2" | "H3" | "H4";
-interface propsFont {
-    tag: headTag,
-    fontSize: number,
-    lineHeight: number,
-    fontWeight: number,
-    children?: string,
-}
+type paragraphTag = "body-L" | "body-M" | "body-M-regular" | "body-S" | "body-S-regular" | "body-XS";
+
 const font = '"Poppins", sans-serif';
-
-function changeStyle({tag, fontSize, lineHeight, fontWeight}: propsFont){
-    const fontShorthand = `font: ${fontWeight} ${fontSize}px/${lineHeight}% ${font};`;
-
-    if(tag === "H1") {
-        const Head = styled.h1`${fontShorthand}`;
-        return Head;
-    }
-
-    if(tag === "H2") {
-        const Head = styled.h2`${fontShorthand}`;
-        return Head;
-    }
-
-    if(tag === "H3") {
-        const Head = styled.h3`${fontShorthand}`;
-        return Head;
-    }
-
-    if(tag === "H4") {
-        const Head = styled.h4`${fontShorthand}`;
-        return Head;
+const styleMapping = {
+    desktop: {
+        'H1': styled.h1`font: 500 44px/128% ${font}`,
+        'H2': styled.h2`font: 500 30px/120% ${font}`,
+        'H3': styled.h3`font: 600 22px/124% ${font}`,
+        'H4': styled.h4`font: 700 18px/132% ${font}`,
+    },
+    tablet: {
+        'H1': styled.h1`font: 500 32px/124% ${font}`,
+        'H2': styled.h2`font: 500 24px/128% ${font}`,
+        'H3': styled.h3`font: 600 22px/124% ${font}`,
+        'H4': styled.h4`font: 700 18px/132% ${font}`,
+    },
+    mobile: {
+        'H1': styled.h1`font: 600 29px/124% ${font}`,
+        'H2': styled.h2`font: 600 22px/128% ${font}`,
+        'H3': styled.h3`font: 700 19px/128% ${font}`,
+        'H4': styled.h4`font: 700 16px/128% ${font}`,
+    },
+    main: {
+       'body-L': styled.p`font: 400 22px/144% ${font}`,
+       'body-M': styled.p`font: 300 18px/132% ${font}`,
+       'body-M-regular': styled.p`font: 400 18px/132% ${font}`,
+       'body-S': styled.p`font: 300 16px/148% ${font}`,
+       'body-S-regular': styled.p`font: 400 16px/148% ${font}`,
+       'body-XS': styled.h1`font: 400 14px/140% ${font}`,
     }
 }
 
-function Typography({ tag, fontSize, lineHeight, fontWeight, children }: propsFont){
-    const Head = changeStyle({tag, fontSize, lineHeight, fontWeight})!;
+function changeStyle(size: size, tag: headTag | paragraphTag) {
+    return styleMapping[size][tag];
+}
 
+function Typography({size, tag, children}: {size: size, tag: headTag | paragraphTag, children: string}){
+    const ResponsiveText = changeStyle(size, tag);
     return(
-        <Head>
+        <ResponsiveText>
             {children}
-        </Head>
+        </ResponsiveText>
     )
 };
 
