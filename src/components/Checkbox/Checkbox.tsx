df2a2180841c7a59edb8dc7e062cac0f2225194f
Matheus Castro <castro.dev37@gmail.com>
2024-10-23 10:56:14 -0300

    ajustes nas props e icone de tick

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,7 +1,5 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { theme } from "../colors/colorts";
-import checked from "../../assets/checked.svg";
-import undefined from "../../assets/Undefined.svg";
 
 type checkboxType = "default" | "disabled" | "error";
 type checkboxState = "empty" | "checked" | "undefined";
@@ -15,78 +13,70 @@ const Container = styled.div`
     gap: 5px;
 `;
 
-const stateCheckboxMapping = {
-    empty: null,
-    checked: {
-        after: css`
-            content: '';
-            position: absolute;
-            left: 3px;
-            top: 9px;
-            width: 6px;
-            height: 2px;
-            background-color: ${theme.corporate.purple};
-            transform: rotate(45deg);
-        `,
-        before: css`
-            display: block;
-            content: '';
-            position: absolute;
-            left: 9px;
-            top: 4px;
-            width: 2px;
-            height: 10px;
-            background-color: ${theme.corporate.purple};
-            transform: rotate(35deg);
-        `
-    },
-    undefined: css`
-        content: '';
-        position: absolute;
-        left: 7px;
-        top: 4px;
-        width: 2px;
-        height: 10px;
-        background-color: ${theme.corporate.purple};
-        transform: rotate(90deg);
-    `
-}
-
-const InputElement = styled.input<{ $state: checkboxState, $type: checkboxType }>`
+const InputElement = styled.input`
     display: none;
-    /* +label:after{
-        ${props => props.$state === "empty" ? stateCheckboxMapping["empty"] : props.$state === "checked" ? stateCheckboxMapping["checked"]["after"] : stateCheckboxMapping["undefined"]}
-        background-color: ${props => props.$type === "default" ? null : props.$type === "disabled" ? theme.grayscale.border : theme.informing.error};
-        }
-        
-        +label:before {
-            ${props => props.$state === "checked" ? stateCheckboxMapping["checked"]["before"] : null};
-            background-color: ${props => props.$type === "default" ? null : props.$type === "disabled" ? theme.grayscale.border : theme.informing.error};
-            } */
-            `;
+`;
 
 const LabelElement = styled.label<{ $type: checkboxType, $state: checkboxState }>`
-    mask: ${props => props.$state === "empty" ? null : props.$state === "checked" ? `url(${checked})` : `url(${undefined})`};
-    mask-repeat: no-repeat;
-    mask-position: center;
-    /* background-repeat: no-repeat;
-    background-position: center; */
-    display: block;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     width: 18px;
     height: 18px;
     border-radius: 4px;
     border: 1px solid ${props => props.$type === "default" ? theme.grayscale.spacer : props.$type === "disabled" ? theme.grayscale.border : theme.informing.error};
     position: relative;
     cursor: pointer;
-
     background-color: ${props => props.$type === "default" ? null : props.$type === "disabled" ? theme.grayscale.disabled : '#E8A1B6'};
+
+    svg path{
+        fill: ${props => props.$type === "default" ? null : props.$type === "disabled" ? theme.grayscale.border : theme.informing.error}
+    }
 `;
+
+function CheckedIcon() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="10"
+        height="10"
+        fill="none"
+        viewBox="0 0 10 10"
+      >
+        <g clipPath="url(#clip0_3231_5)">
+          <path
+            fill="#9D3FE7"
+            d="M3.396 8.582l-3.25-3.25a.5.5 0 010-.707l.708-.707a.5.5 0 01.707 0l2.189 2.19 4.69-4.69a.5.5 0 01.706 0l.708.707a.5.5 0 010 .707l-5.75 5.75a.5.5 0 01-.708 0z"
+          ></path>
+        </g>
+        <defs>
+          <clipPath id="clip0_3231_5">
+            <path fill="#fff" d="M0 0H10V10H0z"></path>
+          </clipPath>
+        </defs>
+      </svg>
+    );
+}
+
+function UndefinedIcon() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="10"
+        height="2"
+        fill="none"
+        viewBox="0 0 10 2"
+      >
+        <path fill="#9D3FE7" d="M0 0H10V2H0z"></path>
+      </svg>
+    );
+  }
 
 function Checkbox({ type, state, ...props }: CheckboxProps){
     return(
         <Container {...props}>
-            <InputElement $type={type} $state={state} id="checkbox" />
-            <LabelElement $type={type} $state={state} htmlFor="checkbox"></LabelElement>
+            <InputElement id="checkbox" />
+            <LabelElement $type={type} $state={state} htmlFor="checkbox">{state === "empty" ? null : state === "checked" ? <CheckedIcon/> : <UndefinedIcon/>}</LabelElement>
         </Container>
     );
 }
