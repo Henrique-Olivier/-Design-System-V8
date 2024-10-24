c60f1539916624f57e848eb8169880d4d6589497
henrique-Olivier <henrique.rosa@sptech.school>
2024-10-24 11:22:41 -0300

    Modal corrigido

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,77 +1,30 @@
-import styled from "styled-components"
-import { theme } from "../colors/colorts"
-import CloseIcon from './assets/close.svg'
-import React from "react"
 
-interface ModalProps {
-    children: React.ReactNode;
-    isVisible: boolean;
-    onClose: () => void;
-} 
+import CloseIcon from "../assets/close.svg";
+import * as Style from "./style.ts"
+import * as Types from "./types.ts"
 
+export default function Modal({
+  children,
+  isVisible,
+  onClose,
+  img,
+  action,
+}: Types.ModalProps) {
+  if (!isVisible) return null;
 
-const Overlay = styled.div`
-    background-color: rgba(26,20,31, 0.7); 
-    width: 100vw; 
-    height: 100vh; 
-    z-index: 9998; 
-    position: fixed;
-    top: 0;
-    left: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    `
-const Container = styled.div`
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    z-index: 9999; 
-    box-sizing: border-box;
-    background-color: ${theme.grayscale.white}; 
-    width: 460px;
-    height: fit-content;
-    padding: 16px;
-    border-radius: 8px; 
-    text-align: end;
-
-    img {
-    width: 16px;
-    height: 16px;
-    } 
-`
-
-const ContainerContent = styled.div`
-    padding: 20px 36px; 
-    width: 100%;  
-    text-align: start;
-`
-
-const CloseButton = styled.button`
-    background-color: transparent;
-    border: none;
-    cursor: pointer; 
-`
-
-
-
-export default function Modal({children, isVisible, onClose} : ModalProps) {
-
-    if(!isVisible) return null;
-
-    return (
-        <>
-        <Overlay onClick={onClose}>
-        </Overlay>
-            <Container>
-                <CloseButton onClick={onClose}>
-                <img src={CloseIcon}/>
-                </CloseButton>
-                <ContainerContent>
-                    {children}
-                </ContainerContent>
-            </Container>
-        </>
-    )
+  return (
+    <>
+      <Style.Overlay onClick={onClose}></Style.Overlay>
+      <Style.Container>
+        <Style.CloseButton onClick={onClose}>
+          <img src={CloseIcon} />
+        </Style.CloseButton>
+        <Style.ContainerContent>
+          {img && <img src={img} />}
+          {children}
+          {action && <Style.DivBtns>{action}</Style.DivBtns>}
+        </Style.ContainerContent>
+      </Style.Container>
+    </>
+  );
 }
