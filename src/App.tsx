f684baf1650dac168d54d1ef3d1215a76713907b
Matheus Castro <castro.dev37@gmail.com>
2024-10-25 08:49:23 -0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,77 @@
 import styled from "styled-components";
-import ProgressBar from "./components/Progress";
+import Input from "./components/Input";
+import icon from "./components/assets/Input_Icon.svg";
+import Checkbox from "./components/Checkbox";
 
+const WrapperInput = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  font-family: "Poppins", sans-serif;
 
-const Teste = styled.div`
-  width: 100px;
-  height: 100px;
-  padding: 10px;
-  border: 1px solid #f1f1f1;
-`
+  >div{
+    display: flex;
+    align-items: flex-end;
+    gap: 20px;
+  }
+`;
+
+const WrapperCheckbox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  margin-top: 80px;
+
+  >div{
+    display: flex;
+    align-items: flex-end;
+    gap: 20px;
+  }
+`;
 
 export default function App () {
 
   return (
-    <>  
-    <Teste>
-      <ProgressBar percents={49} style="percent" type="circle"/>
-    </Teste>
+    <>
+      <WrapperInput>
+        <h2>Input Component</h2>
+        <div>
+          <Input height="default" placeholder="Text" icon={icon} />
+          <Input height="default" placeholder="Text" icon={icon} />
+          <Input height="default" placeholder="Text" icon={icon} disabled />
+          <Input height="default" placeholder="Text" icon={icon} textLabel="Text" />
+          <Input height="default" placeholder="Text" icon={icon} textError="Error" />
+        </div>
+
+        <div>  
+          <Input height="small" placeholder="Text" />
+          <Input height="small" placeholder="Text" />
+          <Input height="small" placeholder="Text" disabled />
+          <Input height="small" placeholder="Text" textLabel="Text" />
+          <Input height="small" placeholder="Text" textError="Error" />
+        </div>
+      </WrapperInput>
+
+      <WrapperCheckbox>
+        <h2>Checkbox Component</h2>
+        <div>
+          <Checkbox id="testando01" />
+          <Checkbox id="testando02" defaultChecked />
+          <Checkbox id="testando03" defaultChecked disabled />
+          <Checkbox id="testando04" defaultChecked error />
+        </div>
+
+        <div>
+          <Checkbox type="undefined" id="testando05"/>
+          <Checkbox type="undefined" id="testando06" defaultChecked />
+          <Checkbox type="undefined" id="testando07" defaultChecked disabled />
+          <Checkbox type="undefined" id="testando08" defaultChecked error />
+        </div>
+      </WrapperCheckbox>
     </>
   )
   }
